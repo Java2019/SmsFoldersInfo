460a9532ab05d33761eedd917429d2b47d68cf63
java2019 <java_2019@mail.ru>
2017-06-26 13:07:10 +0300

    test1

--- app/src/main/java/com/samples/telephony/smsfoldersinfo/SmsFolderInfoActivity.java
+++ app/src/main/java/com/samples/telephony/smsfoldersinfo/SmsFolderInfoActivity.java
@@ -1,13 +1,29 @@
 package com.samples.telephony.smsfoldersinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class SmsFolderInfoActivity extends AppCompatActivity {
+public class SmsFolderInfoActivity extends ListActivity {
+
+    private String[] folders = {"inbox", "send", "draft", "outbox", "failed",
+                                "queued", "undelivered", "conversations"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sms_folder_info);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, folders));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(getApplicationContext(), SmsFolderInfoActivity.class);
+        intent.putExtra("uri", "content://sms/" + folders[position]);
+        startActivity(intent);
     }
 }
