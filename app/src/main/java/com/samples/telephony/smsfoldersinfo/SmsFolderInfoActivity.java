353425585616f9615f01ae9b282bf621f26fcb70
java2019 <java_2019@mail.ru>
2017-06-26 12:57:35 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/telephony/smsfoldersinfo/SmsFolderInfoActivity.java
@@ -0,0 +1,13 @@
+package com.samples.telephony.smsfoldersinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmsFolderInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_folder_info);
+    }
+}
